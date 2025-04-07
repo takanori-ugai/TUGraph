a089123d6d7b3d82c5dd6d401b492d7e6f329b87
Takanori Ugai <ugai@fujitsu.com>
2025-04-07 14:42:33 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,7 @@ spotbugs {
 }
 
 jacoco {
-    toolVersion = "0.8.12"
+    toolVersion = "0.8.13"
 //    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
 }
 
@@ -150,7 +150,7 @@ spotless {
         removeUnusedImports()
 
         // Choose one of these formatters.
-        googleJavaFormat("1.25.2") // has its own section below
+        googleJavaFormat("1.26.0") // has its own section below
         formatAnnotations() // fixes formatting of type annotations, see below
     }
 }
